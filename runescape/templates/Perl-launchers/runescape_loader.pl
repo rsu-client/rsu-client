2d0f68d2262570c46228f1a372006873d4041271
HikariKnight <mu.antilag@gmail.com>
2013-01-22 13:20:00 +0100

    Hide the cmd window that pops up whenever a command is run on windows

--- runescape/templates/Perl-launchers/runescape_loader.pl
+++ runescape/templates/Perl-launchers/runescape_loader.pl
@@ -3,6 +3,15 @@
 # Use FindBin module to get script directory
 use FindBin;
 
+# If no arguments are passed
+if ("@ARGV" =~ //)
+{
+	# Make sure the cmd window gets hidden on windows
+	use Win32::GUI();
+	my $cmdwindow = Win32::GUI::GetPerlWindow();
+	Win32::GUI::Hide($cmdwindow);
+}
+
 # Load the runescape script inside this loader 
 #(if this loader is packaged with PAR::Packer this 
 #will let the perl script run inside the compressed perl)
