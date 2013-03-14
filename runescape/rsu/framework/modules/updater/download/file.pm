de76309b019a307f4ef06d3760994109aa6468d9
HikariKnight <mu.antilag@gmail.com>
2013-03-14 10:38:16 +0100

    enable usage of non gui download even when wx is installed

--- runescape/rsu/framework/modules/updater/download/file.pm
+++ runescape/rsu/framework/modules/updater/download/file.pm
@@ -3,13 +3,26 @@ package updater::download::file;
 sub from
 {
 	# Get the passed data
-	my ($url, $location) = @_;
+	my ($url, $location, $nogui) = @_;
 	
 	# Try and load Wx
 	eval "use Wx";
 	
-	# If Wx is loaded successfully (no errors reported)
-	if (!$@)
+	# Remove warnings as the dependent modules of LWP outputs quite a few which can be ignored
+	no strict;
+	no warnings;
+	
+	# If Wx is not loaded or $nogui is 1 then
+	if (($@) || (defined $nogui && $nogui eq '1'))
+	{
+		# Use the fallback download using LWP and output only to STDOUT
+		require updater::download::sysdload;
+
+		# Run the commands download the file
+		updater::download::sysdload::sysdownload($url,$location);
+	}
+	# Else if Wx is loaded successfully (no errors reported)
+	else
 	{
 		# Load the native gui to download
 		require updater::download::wxdload;
@@ -17,14 +30,10 @@ sub from
 		# Download with LWP and show output in a Wx window
 		updater::download::wxdload::wxdownload($url,$location);
 	}
-	else
-	{
-		# Use the fallback download (using wget or curl and possibly show output in zenity)
-		require updater::download::sysdload;
-
-		# Run the commands download the file
-		updater::download::sysdload::sysdownload($url,$location);
-	}
+	
+	# Enable warnings again
+	use strict;
+	use warnings;
 }
 
 #
