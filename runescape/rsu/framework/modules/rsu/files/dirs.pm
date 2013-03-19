65b97da5bbf6ba357fe1232c3ade787fbf1afd26
HikariKnight <mu.antilag@gmail.com>
2013-03-19 13:33:59 +0100

    try to fix the issue where the list and rlist functions dont work on mac and older linux versions

--- runescape/rsu/framework/modules/rsu/files/dirs.pm
+++ runescape/rsu/framework/modules/rsu/files/dirs.pm
@@ -5,20 +5,48 @@ sub list
 	# Get the passed data
 	my ($dir) = @_;
 	
-	# Open the directory
-	opendir(my $dir_content, $dir);
+	# Make a variable to contain a value that tells us if we need to use fallback methods or not
+	my $fallback = 0;
+	
+	# Check if functions we need are supported and put fallback to 1 if they are not supported
+	eval "use 5.0.12"; $fallback = 1 if $@;
 	
 	# Make an array to contain the files inside
 	my @list;
 	
-	# While there are still contents in the directory
-	while (readdir $dir_content)
+	# If the functions we need are not supported then
+	if ($fallback eq '1')
 	{
-		# Skip . and anything that starts with a dot
-		next if $_ =~ /^\./;
+		# List the files in $dir
+		my $dirlist = `ls "$dir"`;
 		
-		# Add file to array
-		push @list, $_;
+		# Split the files by newline into an array
+		my @dir = split /\n/, $dirlist;
+		
+		# For each value in the array
+		foreach my $file (@dir)
+		{
+			# Skip . and anything that starts with a dot
+			next if $file =~ /^\./;
+			
+			# Add file to array
+			push @list, $file;
+		}
+	}
+	else
+	{
+		# Open the directory
+		opendir(my $dir_content, $dir);
+		
+		# While there are still contents in the directory
+		while (readdir $dir_content)
+		{
+			# Skip . and anything that starts with a dot
+			next if $_ =~ /^\./;
+			
+			# Add file to array
+			push @list, $_;
+		}
 	}
 	
 	# Return the array
@@ -35,36 +63,77 @@ sub rlist
 	# Get the passed data
 	my ($dir) = @_;
 	
-	# Open the directory
-	opendir(my $dir_content, $dir);
+	# Make a variable to contain a value that tells us if we need to use fallback methods or not
+	my $fallback = 0;
+	
+	# Check if functions we need are supported and put fallback to 1 if they are not supported
+	eval "use 5.0.12"; $fallback = 1 if $@;
 	
 	# Make an array to contain the files inside
 	my @list;
 	
-	# While there are still contents in the directory
-	while (readdir $dir_content)
+	# If the functions we need are not supported then
+	if ($fallback eq '1')
 	{
-		# Go to next if current file is . .. .DS_Store or .directory
-		next if $_ =~ /^(\.|\.\.|\.DS_Store|\.directory)/i;
+		# List the files in $dir
+		my $dirlist = `ls "$dir"`;
 		
-		# Make a variable to contain the filename
-		my $filename = $_;
+		# Split the files by newline into an array
+		my @dir = split /\n/, $dirlist;
 		
-		# If the current object does not start with a . and is a directory
-		if (-d "$dir/$filename")
+		# For each value in the array
+		foreach my $file (@dir)
 		{
-			# Get all the recursive files
-			my @recursive = rsu::files::dirs::rlist("$dir/$filename");
+			# Skip . and anything that starts with a dot
+			next if $file =~ /^\./;
 			
-			# Merge the arrays
-			@list = (@list, @recursive);
+			# If the current object does not start with a . and is a directory
+			if (-d "$dir/$file")
+			{
+				# Get all the recursive files
+				my @recursive = rsu::files::dirs::rlist("$dir/$file");
+				
+				# Merge the arrays
+				@list = (@list, @recursive);
+				
+				# Go to next loop
+				next;
+			}
 			
-			# Go to next loop
-			next;
+			# Add file to array
+			push @list, "$dir/$file";
 		}
+	}
+	else
+	{
+		# Open the directory
+		opendir(my $dir_content, $dir);
 		
-		# Add file to array
-		push @list, "$dir/$_";
+		# While there are still contents in the directory
+		while (readdir $dir_content)
+		{
+			# Go to next if current file is . .. .DS_Store or .directory
+			next if $_ =~ /^(\.|\.\.|\.DS_Store|\.directory)/i;
+			
+			# Make a variable to contain the filename
+			my $filename = $_;
+			
+			# If the current object does not start with a . and is a directory
+			if (-d "$dir/$filename")
+			{
+				# Get all the recursive files
+				my @recursive = rsu::files::dirs::rlist("$dir/$filename");
+				
+				# Merge the arrays
+				@list = (@list, @recursive);
+				
+				# Go to next loop
+				next;
+			}
+			
+			# Add file to array
+			push @list, "$dir/$_";
+		}
 	}
 	
 	# Return the array
