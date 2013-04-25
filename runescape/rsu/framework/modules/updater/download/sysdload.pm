84f58473764a04aab7dda8ff1a572ed476754f7e
hikariknight <mu.antilag@gmail.com>
2013-04-25 16:51:50 +0200

    readurl will have a default connection timeout of 30 seconds

--- runescape/rsu/framework/modules/updater/download/sysdload.pm
+++ runescape/rsu/framework/modules/updater/download/sysdload.pm
@@ -64,14 +64,14 @@ sub readurl
 		if(`ls /usr/bin | grep wget` =~  /wget/)
 		{
 			# Use wget command to fetch files
-			$fetchcommand = "wget -q -O-" if !defined $timeout;
+			$fetchcommand = "wget -q --connect-timeout=30 -O-" if !defined $timeout;
 			$fetchcommand = "wget -q --connect-timeout=$timeout --timeout=$timeout -O-" if defined $timeout;
 		}
 		# Else if /usr/bin contains curl
 		elsif(`ls /usr/bin | grep curl` =~  /curl/)
 		{
 			# Curl command equalent to the wget command to fetch files
-			$fetchcommand = "curl -L -#" if !defined $timeout;
+			$fetchcommand = "curl -L --connect-timeout 30 -#" if !defined $timeout;
 			$fetchcommand = "curl -L --connect-timeout $timeout -m $timeout -#" if defined $timeout;
 		}
 
