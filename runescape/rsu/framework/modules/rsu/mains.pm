bc52aa82980ab44863868efca2d0f903c76afacf
hikariknight <mu.antilag@gmail.com>
2013-04-25 19:09:31 +0200

    fix the windows main

--- runescape/rsu/framework/modules/rsu/mains.pm
+++ runescape/rsu/framework/modules/rsu/mains.pm
@@ -249,7 +249,7 @@ sub windows_main
 	#system "set PATH=$javalibspath;%PATH% && \"$win32javabin\" ".$rsu_data->verboseprms." -cp  $params /share 2>&1";
 	
 	# Run the jar file
-	rsu::mains::runjar("set PATH=$javalibspath;%PATH% && $win32javabin ".$rsu_data->verboseprms." -cp  $params \"$parentfolder[-1]/share/img\" 2>&1");
+	rsu::mains::runjar("set PATH=$javalibspath;%PATH% && \"$win32javabin\" ".$rsu_data->verboseprms." -cp  $params \"$parentfolder[-1]/share/img\" 2>&1");
 }
 
 #
