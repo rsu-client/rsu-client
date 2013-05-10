b09979d8880da6f28a19958f38b64f7b4ec364e4
HikariKnight <mu.antilag@gmail.com>
2013-05-10 08:31:04 +0200

    made it possible to toggle the optimizejava prms from the optimizejava=true|false switch in the command line.
    will add it to the settings gui soon

--- runescape/rsu/framework/modules/rsu/java/optimizer.pm
+++ runescape/rsu/framework/modules/rsu/java/optimizer.pm
@@ -5,7 +5,11 @@ sub run
 	# Get the passed data
 	my ($javabin, $params) = @_;
 	
+	# Tell the user what we are doing
+	print "Adding optimization parameters to Java.\nParameters that will be added are:\n-XX:+AggressiveOpts -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+TieredCompilation -XX:ReservedCodeCacheSize=256m -XX:+UseAdaptiveGCBoundary -XX:SurvivorRatio=16 -XX:+UseParallelGC\nNOTE: If any of these are added to the prm file the parameter from the prm file will be used instead!\n\n";
+	
 	# Optimize the Java execution
+	$javabin = "$javabin -XX:+AggressiveOpts" if $params !~ /-XX:+AggressiveOpts/;
 	$javabin = "$javabin -XX:+UnlockExperimentalVMOptions" if $params !~ /-XX:+UnlockExperimentalVMOptions/;
 	$javabin = "$javabin -XX:+DisableExplicitGC" if $params !~ /-XX:+DisableExplicitGC/;
 	$javabin = "$javabin -XX:+TieredCompilation" if $params !~ /-XX:+TieredCompilation/;
